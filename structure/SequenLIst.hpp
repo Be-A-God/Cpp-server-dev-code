150dcceac544fe8c207b0a12950063fe03097e6e
ubuntu@Be-A-God <wx1028674648@gmail.com>
2022-02-25 18:51:55 +0800

    update documentation

--- structure/SequenLIst.hpp
+++ structure/SequenLIst.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file SequenLIst.hpp
+ * @author Be-A-God (wx1028674648@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-02-25
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #pragma once
 #include <concepts>
 #include <string>
--- structure/SequenList.cpp
+++ structure/SequenList.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file SequenList.cpp
+ * @author Be-A-God (wx1028674648@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-02-25
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #include "SequenLIst.hpp"
 
 template <class data_type, int elem_nums>
