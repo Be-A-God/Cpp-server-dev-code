c11487b61f6f810e78bf2aefe92053cb2a494066
ubuntu@Be-A-God <wx1028674648@gmail.com>
2022-02-28 17:45:39 +0800

    create & destory & find

--- data_structure/linear_table/linked_list/singly_linked_list.hpp
+++ data_structure/linear_table/linked_list/singly_linked_list.hpp
@@ -32,10 +32,10 @@ requires(std::is_same<elem_type, int>::value ||
   singly_linked_list<elem_type> &operator=(singly_linked_list<elem_type> &&) =
       delete;
 
-  Node<elem_type> *find_elem(elem_type value);
-  Node<elem_type> *find_countdown_k(int k);
-  Node<elem_type> *find_middle();
-  elem_type get_elem(int pos);
+  const Node<elem_type> *find_elem const(elem_type value);
+  const Node<elem_type> *find_countdown_k const(int k);
+  const Node<elem_type> *find_middle const();
+  elem_type get_elem const(int pos);
   bool create_head_plug();
   bool create_tail_plug();
   bool delete_repeat();
@@ -44,11 +44,107 @@ requires(std::is_same<elem_type, int>::value ||
   bool insert_elem(int pos, elem_type value);
   bool delete_elem(elem_type value);
   bool delete_elem(int pos);
-  bool is_empty();
-  int get_length();
-  void travel_list();
+  bool is_empty const();
+  int get_length const();
+  void travel_list const();
   //   void merge_order_list(singly_linked_list<elem_type> &other_list);
 
  private:
   Node<elem_type> *head_node_point;
 };
+
+template <class elem_type>
+singly_linked_list<elem_type>::singly_linked_list() {
+  head_node_point = new Node<elem_type>;
+
+  if (!head_node_point) {
+    std::cout << "链表创建失败！" << std::endl;
+    ~singly_linked_list();
+    exit(1);
+  }
+
+  head_node_point->next = nullptr;
+  head_node_point->data = 0;
+
+  std::cout << "链表创建成功" << std::endl;
+}
+
+template <class elem_type>
+singly_linked_list<elem_type>::~singly_linked_list() {
+  if (head_node_point) {
+    Node<elem_type> *ptr = head_node_point->next;
+    Node<elem_type> *temp_ptr;
+
+    while (ptr) {
+      temp_ptr = ptr;
+      ptr = ptr->next;
+      temp_ptr->next = nullptr;
+      delete temp_ptr;
+    }
+
+    head_node_point->next = nullptr;
+    delete head_node_point;
+
+    std::cout << "链表销毁成功" << std::endl;
+  }
+}
+
+template <class elem_type>
+const Node<elem_type> *singly_linked_list<elem_type>::find_elem const(
+    elem_type value) {
+  if (head_node_point->next) {
+    Node<elem_type> *temp_ptr = head_node_point->next;
+
+    while (temp_ptr) {
+      if (temp_ptr->data == value) {
+        return temp_ptr;
+      }
+      temp_ptr = temp_ptr->next;
+    }
+  }
+  std::cout << "查找元素失败：链表中没有该元素值！" << std::endl;
+  return nullptr;
+}
+
+template <class elem_type>
+const Node<elem_type> *singly_linked_list<elem_type>::find_countdown_k(
+    int k) const {}
+
+template <class elem_type>
+const Node<elem_type> *singly_linked_list<elem_type>::find_middle() const {}
+
+template <class elem_type>
+elem_type singly_linked_list<elem_type>::get_elem(int pos) const {}
+
+template <class elem_type>
+bool singly_linked_list<elem_type>::create_head_plug() {}
+
+template <class elem_type>
+bool singly_linked_list<elem_type>::create_tail_plug() {}
+
+template <class elem_type>
+bool singly_linked_list<elem_type>::delete_repeat() {}
+
+template <class elem_type>
+bool singly_linked_list<elem_type>::reverse_list() {}
+
+template <class elem_type>
+bool singly_linked_list<elem_type>::empty_list() {}
+
+template <class elem_type>
+bool singly_linked_list<elem_type>::insert_elem(int pos, elem_type value) {}
+
+template <class elem_type>
+bool singly_linked_list<elem_type>::delete_elem(elem_type value) {}
+
+template <class elem_type>
+bool singly_linked_list<elem_type>::delete_elem(int pos) {}
+
+template <class elem_type>
+bool singly_linked_list<elem_type>::is_empty() const {}
+
+template <class elem_type>
+int singly_linked_list<elem_type>::get_length() const {}
+
+template <class elem_type>
+void singly_linked_list<elem_type>::travel_list() const {}
