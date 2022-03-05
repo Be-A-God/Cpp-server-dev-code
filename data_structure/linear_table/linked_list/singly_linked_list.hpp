e173b5a560a40d4df6257067b8e386b5ceb8f4a4
ubuntu@Be-A-God <wx1028674648@gmail.com>
2022-03-05 22:16:43 +0800

    class finished

--- data_structure/linear_table/linked_list/singly_linked_list.hpp
+++ data_structure/linear_table/linked_list/singly_linked_list.hpp
@@ -36,17 +36,17 @@ requires(std::is_same<elem_type, int>::value ||
   const Node<elem_type> *find_countdown_k(int k) const;
   const Node<elem_type> *find_middle() const;
   elem_type get_elem(int pos) const;
-  bool delete_repeat();
-  bool reverse_list();
-  bool empty_list();
-  bool insert_elem(int pos, elem_type value);
-  bool delete_elem(elem_type value);
-  bool delete_elem(int pos);
-  bool is_empty() const;
-  int get_length() const;
+  void delete_abs_repeat();
+  void reverse_list();
+  void empty_list();
+  void insert_elem(int pos, elem_type value);
+  void delete_elem(elem_type value);
+  void delete_elem(int pos);
   void travel_list() const;
   void create_head_plug();
   void create_tail_plug();
+  bool is_empty() const;
+  int get_length() const;
   //   void merge_order_list(singly_linked_list<elem_type> &other_list);
 
  private:
@@ -118,10 +118,6 @@ const Node<elem_type> *singly_linked_list<elem_type>::find_countdown_k(
 
 template <class elem_type>
 const Node<elem_type> *singly_linked_list<elem_type>::find_middle() const {
-  if (!head_node_point->next) {
-    return nullptr;
-  }
-
   Node<elem_type> *fast_ptr = head_node_point;
   Node<elem_type> *slow_ptr = fast_ptr;
 
@@ -131,7 +127,7 @@ const Node<elem_type> *singly_linked_list<elem_type>::find_middle() const {
     slow_ptr = slow_ptr->next;
   }
 
-  return slow_ptr;
+  return fast_ptr == slow_ptr ? nullptr : slow_ptr;
 }
 
 template <class elem_type>
@@ -153,7 +149,7 @@ template <class elem_type>
 void singly_linked_list<elem_type>::create_head_plug() {
   int elem_nums = 0;
   std::cout << "请输入插入的元素个数：" << std::endl;
-  while (!(std::cin >> elem_nums) && elem_nums <= 0) {
+  while (!(std::cin >> elem_nums) && elem_nums < 1) {
     std::cout << "元素个数输入有误，请重新输入！" << std::endl;
   }
 
@@ -177,7 +173,7 @@ void singly_linked_list<elem_type>::create_tail_plug() {
   int elem_nums = 0;
 
   std::cout << "请输入插入的元素个数：" << std::endl;
-  while (!(std::cin >> elem_nums) && elem_nums <= 0) {
+  while (!(std::cin >> elem_nums) && elem_nums < 1) {
     std::cout << "元素个数输入有误，请重新输入！" << std::endl;
   }
 
@@ -193,28 +189,158 @@ void singly_linked_list<elem_type>::create_tail_plug() {
 }
 
 template <class elem_type>
-bool singly_linked_list<elem_type>::delete_repeat() {}
+void singly_linked_list<elem_type>::delete_abs_repeat() {
+  Node<elem_type> *pre_ptr = head_node_point;
+  Node<elem_type> *del_ptr = nullptr;
+  elem_type index = 0;
+
+  while (pre_ptr->next) {
+    pre_ptr = pre_ptr->next;
+    if (abs(pre_ptr->data) > index) {
+      index = pre_ptr->data;
+    }
+  }
+
+  return index ? []() -> { std::cout << "链表为空！无重复元素！" << std::endl; }
+               : [&]() mutable -> {
+                   pre_ptr = head_node_point;
+                   int *flag = new int[index + 1]{0};
+                   while (pre_ptr->next) {
+                     if (!flag[pre_ptr->next->data]) {
+                       flag[pre_ptr->next->data] = 1;
+                       pre_ptr = pre_ptr->next;
+                     } else {
+                       del_ptr = pre_ptr->next;
+                       pre_ptr->next = del_ptr->next;
+                       del_ptr->next = nullptr;
+                       delete del_ptr;
+                     }
+                   }
+                   delete[] flag;
+                   std::cout << "删除重复元素完成！" << std::endl;
+                 };
+}
 
 template <class elem_type>
-bool singly_linked_list<elem_type>::reverse_list() {}
+void singly_linked_list<elem_type>::reverse_list() {
+  Node<elem_type> *cur_ptr = head_node_point->next;
+  Node<elem_type> *back_ptr = cur_ptr;
+  head_node_point->next = nullptr;
+
+  while (cur_ptr) {
+    back_ptr = cur_ptr->next;
+    cur_ptr->next = head_node_point->next;
+    head_node_point->next = cur_ptr;
+    cur_ptr = back_ptr;
+  }
+
+  std::cout << "链表翻转完毕！" << std::endl;
+}
 
 template <class elem_type>
-bool singly_linked_list<elem_type>::empty_list() {}
+void singly_linked_list<elem_type>::empty_list() {
+  Node<elem_type> *cur_ptr = head_node_point->next;
+  Node<elem_type> *back_ptr = cur_ptr;
+  head_node_point->next = nullptr;
+
+  while (cur_ptr) {
+    back_ptr = cur_ptr->next;
+    cur_ptr->next = nullptr;
+    delete cur_ptr;
+    cur_ptr = back_ptr;
+  }
+
+  std::cout << "链表清空完毕（不含首元结点）!" << std::endl;
+}
 
 template <class elem_type>
-bool singly_linked_list<elem_type>::insert_elem(int pos, elem_type value) {}
+void singly_linked_list<elem_type>::insert_elem(int pos, elem_type value) {
+  Node<elem_type> *pre_ptr = head_node_point;
+  Node<elem_type> *ins_ptr = nullptr;
+  int index = 1;
+
+  while (index < pos && pre_ptr) {
+    pre_ptr = pre_ptr->next;
+    index++;
+  }
+
+  if (!pre_ptr || index > pos) {
+    std::cout << "插入位置有误！" << std::endl;
+    exit(-1);
+  } else {
+    ins_ptr = new Node<elem_type>;
+    ins_ptr->data = value;
+    ins_ptr->next = pre_ptr->next;
+    pre_ptr->next = ins_ptr;
+    std::cout << "插入完成！" << std::endl;
+  }
+}
 
 template <class elem_type>
-bool singly_linked_list<elem_type>::delete_elem(elem_type value) {}
+void singly_linked_list<elem_type>::delete_elem(elem_type value) {
+  Node<elem_type> *cur_ptr = head_node_point->next;
+  Node<elem_type> *pre_ptr = nullptr;
+
+  while (cur_ptr && cur_ptr->data != value) {
+    pre_ptr = cur_ptr;
+    cur_ptr = cur_ptr->next;
+  }
+
+  if (!cur_ptr) {
+    std::cout << "找不到该元素，删除失败！" << std::endl;
+  } else {
+    pre_ptr->next = cur_ptr->next;
+    cur_ptr->next = nullptr;
+    delete cur_ptr;
+    std::cout << "元素删除成功！" << std::endl;
+  }
+}
 
 template <class elem_type>
-bool singly_linked_list<elem_type>::delete_elem(int pos) {}
+void singly_linked_list<elem_type>::delete_elem(int pos) {
+  Node<elem_type> *pre_ptr = head_node_point;
+  Node<elem_type> *cur_ptr = head_node_point->next;
+
+  while (cur_ptr && --pos > 0) {
+    pre_ptr = cur_ptr;
+    cur_ptr = cur_ptr->next;
+  }
+
+  if (!cur_ptr || pos < 0) {
+    std::cout << "删除位置有误或者链表为空！" << std::endl;
+    exit(-1);
+  } else {
+    pre_ptr->next = cur_ptr->next;
+    cur_ptr->next = nullptr;
+    delete cur_ptr;
+    std::cout << "删除元素成功！" << std::endl;
+  }
+}
 
 template <class elem_type>
-bool singly_linked_list<elem_type>::is_empty() const {}
+bool singly_linked_list<elem_type>::is_empty() const {
+  return head_node_point->next ? false : true;
+}
 
 template <class elem_type>
-int singly_linked_list<elem_type>::get_length() const {}
+int singly_linked_list<elem_type>::get_length() const {
+  int len = 0;
+  Node<elem_type> *cur_ptr = head_node_point->next;
+
+  while (cur_ptr) {
+    cur_ptr = cur_ptr->next;
+    len++;
+  }
+
+  return len;
+}
 
 template <class elem_type>
-void singly_linked_list<elem_type>::travel_list() const {}
+void singly_linked_list<elem_type>::travel_list() const {
+  Node<elem_type> *cur_ptr = head_node_point->next;
+
+  while (cur_ptr) {
+    std::cout << cur_ptr->data << '\t' << std::endl;
+    cur_ptr = cur_ptr->next;
+  }
+}
