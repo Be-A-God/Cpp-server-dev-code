0e9100d8f12cbbb28f262fc5938f52c468d890fe
ubuntu@Be-A-God <wx1028674648@gmail.com>
2022-03-05 16:06:13 +0800

    Algorithm comparison

--- data_structure/linear_table/linked_list/singly_linked_list.hpp
+++ data_structure/linear_table/linked_list/singly_linked_list.hpp
@@ -36,8 +36,6 @@ requires(std::is_same<elem_type, int>::value ||
   const Node<elem_type> *find_countdown_k(int k) const;
   const Node<elem_type> *find_middle() const;
   elem_type get_elem(int pos) const;
-  bool create_head_plug();
-  bool create_tail_plug();
   bool delete_repeat();
   bool reverse_list();
   bool empty_list();
@@ -47,6 +45,8 @@ requires(std::is_same<elem_type, int>::value ||
   bool is_empty() const;
   int get_length() const;
   void travel_list() const;
+  void create_head_plug();
+  void create_tail_plug();
   //   void merge_order_list(singly_linked_list<elem_type> &other_list);
 
  private:
@@ -54,12 +54,12 @@ requires(std::is_same<elem_type, int>::value ||
 };
 
 template <class elem_type>
-singly_linked_list<elem_type>::singly_linked_list() {
+singly_linked_list<elem_type>::singly_linked_list() : head_node_point(nullptr) {
   head_node_point = new Node<elem_type>;
 
   if (!head_node_point) {
     std::cout << "链表创建失败！" << std::endl;
-    ~singly_linked_list();
+    // ~singly_linked_list();
     exit(1);
   }
 
@@ -71,56 +71,166 @@ singly_linked_list<elem_type>::singly_linked_list() {
 
 template <class elem_type>
 singly_linked_list<elem_type>::~singly_linked_list() {
-  if (head_node_point) {
-    Node<elem_type> *ptr = head_node_point->next;
-    Node<elem_type> *temp_ptr;
+  Node<elem_type> *cur_ptr = head_node_point->next;
+  Node<elem_type> *temp_ptr;
 
-    while (ptr) {
-      temp_ptr = ptr;
-      ptr = ptr->next;
-      temp_ptr->next = nullptr;
-      delete temp_ptr;
-    }
+  head_node_point->next = nullptr;
+  delete head_node_point;
 
-    head_node_point->next = nullptr;
-    delete head_node_point;
-
-    std::cout << "链表销毁成功" << std::endl;
+  while (cur_ptr) {
+    temp_ptr = cur_ptr;
+    cur_ptr = cur_ptr->next;
+    temp_ptr->next = nullptr;
+    delete temp_ptr;
   }
+
+  std::cout << "链表销毁成功" << std::endl;
 }
 
 template <class elem_type>
 const Node<elem_type> *singly_linked_list<elem_type>::find_elem(
     elem_type value) const {
-  if (head_node_point->next) {
-    Node<elem_type> *temp_ptr = head_node_point->next;
+  // if (head_node_point->next) {
+  //   Node<elem_type> *temp_ptr = head_node_point->next;
 
-    while (temp_ptr) {
-      if (temp_ptr->data == value) {
-        return temp_ptr;
-      }
-      temp_ptr = temp_ptr->next;
-    }
+  //   while (temp_ptr) {
+  //     if (temp_ptr->data == value) {
+  //       return temp_ptr;
+  //     }
+  //     temp_ptr = temp_ptr->next;
+  //   }
+  // }
+  // std::cout << "查找元素失败：链表中没有该元素值！" << std::endl;
+  // return nullptr;
+
+  // 优化
+  Node<elem_type> *temp_ptr = head_node_point->next;
+  while (temp_ptr && temp_ptr->data != value) {
+    temp_ptr = temp_ptr->next;
   }
-  std::cout << "查找元素失败：链表中没有该元素值！" << std::endl;
-  return nullptr;
+  return temp_ptr ? temp_ptr : []() -> {
+    std::cout << "查找元素失败：链表中没有该元素值！" << std::endl;
+    return nullptr;
+  };
 }
 
 template <class elem_type>
 const Node<elem_type> *singly_linked_list<elem_type>::find_countdown_k(
-    int k) const {}
+    int k) const {
+  Node<elem_type> *fast_ptr = head_node_point->next;
+  Node<elem_type> *slow_ptr = fast_ptr;
+
+  // 更简洁的做法
+  while (fast_ptr->next) {
+    if (--k <= 0) {
+      slow_ptr = slow_ptr->next;
+    }
+    fast_ptr = fast_ptr->next;
+  }
+  return k > 0 ? nullptr : slow_ptr;
+
+  // for (int i = 1; fast_ptr && i < k; i++) {
+  //   fast_ptr = fast_ptr->next;
+  // }
+
+  // return !fast_ptr ? fast_ptr : [&]() mutable -> Node<elem_type> * {
+  //   while (fast_ptr->next) {
+  //     fast_ptr = fast_ptr->next;
+  //     slow_ptr = slow_ptr->next;
+  //   }
+  //   return slow_ptr;
+  // };
+}
 
 template <class elem_type>
-const Node<elem_type> *singly_linked_list<elem_type>::find_middle() const {}
+const Node<elem_type> *singly_linked_list<elem_type>::find_middle() const {
+  if (!head_node_point->next) {
+    return nullptr;
+  }
+
+  Node<elem_type> *fast_ptr = head_node_point;
+  Node<elem_type> *slow_ptr = fast_ptr;
+
+  while (fast_ptr && fast_ptr->next) {
+    fast_ptr = fast_ptr->next;
+    fast_ptr = fast_ptr->next;
+    slow_ptr = slow_ptr->next;
+  }
+
+  return slow_ptr;
+}
 
 template <class elem_type>
-elem_type singly_linked_list<elem_type>::get_elem(int pos) const {}
+elem_type singly_linked_list<elem_type>::get_elem(int pos) const {
+  // if (pos <= 0 || !head_node_point->next) {
+  //   std::cout << "链表不存在该位置！" << std::endl;
+  //   return -1;
+  // }
+
+  // Node<elem_type> *temp_ptr = head_node_point;
+
+  // while (temp_ptr->next && --pos >= 0) {
+  //   temp_ptr = temp_ptr->next;
+  // }
+
+  // return pos > 0 ? nullptr : temp_ptr;
+
+  // 优化
+  int i = 1;
+  Node<elem_type> *cur_ptr = head_node_point->next;
+
+  while (cur_ptr && i < pos) {
+    cur_ptr = cur_ptr->next;
+    i++;
+  }
+
+  return (i > pos || !cur_ptr)
+         ? []() -> elem_type { std::cout << "链表不存在该位置！" << std::endl; return -1; }
+         : cur_ptr->data;
+}
 
 template <class elem_type>
-bool singly_linked_list<elem_type>::create_head_plug() {}
+void singly_linked_list<elem_type>::create_head_plug() {
+  int elem_nums = 0;
+  std::cout << "请输入插入的元素个数：" << std::endl;
+  while (!(std::cin >> elem_nums) && elem_nums <= 0) {
+    std::cout << "元素个数输入有误，请重新输入！" << std::endl;
+  }
+
+  std::cout << "[头插法创建链表]请依次输入元素:" << std::endl;
+
+  while (--elem_nums >= 0) {
+    Node<elem_type> *temp_ptr = new Node<elem_type>;
+    std::cin >> temp_ptr->data;
+    temp_ptr->next = head_node_point->next;
+    head_node_point->next = temp_ptr;
+  }
+}
 
 template <class elem_type>
-bool singly_linked_list<elem_type>::create_tail_plug() {}
+void singly_linked_list<elem_type>::create_tail_plug() {
+  Node<elem_type> *tail_ptr = head_node_point;
+  while (!tail_ptr->next) {
+    tail_ptr = tail_ptr->next;
+  }
+
+  int elem_nums = 0;
+
+  std::cout << "请输入插入的元素个数：" << std::endl;
+  while (!(std::cin >> elem_nums) && elem_nums <= 0) {
+    std::cout << "元素个数输入有误，请重新输入！" << std::endl;
+  }
+
+  std::cout << "[尾插法创建链表]请依次输入元素:" << std::endl;
+
+  while (--elem_nums >= 0) {
+    Node<elem_type> *temp_ptr = new Node<elem_type>;
+    std::cin >> temp_ptr->data;
+    temp_ptr->next = nullptr;
+    tail_ptr->next = temp_ptr;
+    tail_ptr = temp_ptr;
+  }
+}
 
 template <class elem_type>
 bool singly_linked_list<elem_type>::delete_repeat() {}
