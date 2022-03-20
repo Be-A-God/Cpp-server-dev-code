17d6498003c71f424e345c203af3de679387ec71
ubuntu@Be-A-God <wx1028674648@gmail.com>
2022-03-20 15:22:33 +0800

    proj waiting to be completed

---
+++ GoF23/Instance/Log_system.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file Log_system.cpp
+ * @author Be-A-God (wx1028674648@gmail.com)
+ * @brief
+ * 日志系统，基于C++17版本，采用单例模式，基于NanoLog-----等待作为一个项目实现
+ * @version 0.1
+ * @date 2022-03-20
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
